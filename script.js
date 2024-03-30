d424517a490fa10f0c83510ceff68a34f1c944ae
Cod3Uchiha <157640942+Cod3Uchiha@users.noreply.github.com>
2024-03-30 14:43:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('whatsappButton').addEventListener('click', function() {
+    window.open('https://wa.me/0775571820, '_blank');
+    // Replace '0775571820' with the actual WhatsApp number to open a chat with the business owner
+});
