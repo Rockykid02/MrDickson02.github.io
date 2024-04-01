3145a66dfb264a2c4b8a061dda714ad89c71b570
Rockykid02 <144491319+Rockykid02@users.noreply.github.com>
2024-04-01 23:27:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('whatsappButton').addEventListener('click', function() {
-    window.open('https://wa.me/0775571820, '_blank');
+    window.open('https://wa.me/0780597802, '_blank');
     // Replace '0775571820' with the actual WhatsApp number to open a chat with the business owner
 });
